7905dd0649c763e12bceed99b30ba1a39bb4a139
Devin W. Leaman <dleaman212@gmail.com>
2019-11-30 11:05:19 -0600

    Dumb change for Codacy, oh well

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { CommandoClient, SQLiteProvider } = require('discord.js-commando')
-const { sendMessage } = require('./src/cmds/BaseCmd')
+const sendMessage = require('./src/cmds/BaseCmd').sendMessage
 const config = require('./src/utils/config')
 const logger = require('./src/utils/logger')
 const Tools = require('./src/utils/Tools')
