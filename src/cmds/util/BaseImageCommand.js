0d7c532489adb0bcb7d90fef70304ab7ade7d8e4
Devin W. Leaman <dleaman212@gmail.com>
2019-12-04 03:02:41 -0600

    Updated the BaseImageCommand to make it easier to make future commands

--- src/cmds/util/BaseImageCommand.js
+++ src/cmds/util/BaseImageCommand.js
@@ -6,6 +6,11 @@ class BaseImageCommand extends BaseCmd {
     if (imageIndex && !isNaN(imageIndex) && imageIndex >= 0) {
       return needle('get', `https://ansel.4lch4.com/reaction?name=${commandName}&index=${imageIndex}`)
     } else return needle('get', `https://ansel.4lch4.com/reaction?name=${commandName}`)
+  async run (msg, args) {
+    const image = await this.getCommandImage(msg.command.name, args)
+
+    if (image.body.length > 0) return BaseCmd.sendMessage(msg.channel, '', this.client.user, { files: [image.body] })
+    else return BaseCmd.sendMessage(msg.channel, 'Unfortunately, I do not have any images for this command. Please let `Alcha#0042` know of this problem so he can fix it right away.')
   }
 }
 
