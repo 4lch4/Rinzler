f33be3b03c321bb67a9a3b092923fcea00a6382f
Devin W. Leaman <dleaman212@gmail.com>
2020-01-20 12:32:41 -0600

    Added the Bitcoin command

---
+++ src/cmds/utils/Coinbase.js
@@ -0,0 +1,39 @@
+const config = require('../../utils/config')
+
+const Coinbase = require('coinbase').Client
+const coinbase = new Coinbase({
+  apiKey: config.coinbase.apiKey,
+  apiSecret: config.coinbase.apiSecret,
+  version: '2018-01-17'
+})
+
+const currencies = require('../../../data/currencies.json')
+
+class CoinbaseAid {
+  getCurrentPrice (currency = 'USD') {
+    return new Promise((resolve, reject) => {
+      coinbase.getSpotPrice({ currency: currency }, (err, res) => {
+        if (err) reject(err)
+        else resolve(res.data.amount)
+      })
+    })
+  }
+
+  getHistoricPrice (date, currency = 'USD') {
+    return new Promise((resolve, reject) => {
+      coinbase.getSpotPrice({ date: date, currency: currency }, (err, res) => {
+        if (err) reject(err)
+        else resolve(res.data.amount)
+      })
+    })
+  }
+
+  /**
+   * @returns {Array<String>}
+   */
+  get currencies () {
+    return currencies.data
+  }
+}
+
+module.exports = CoinbaseAid
